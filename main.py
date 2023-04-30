681c147fe87b5ff5ab77fbe6cb30c5a970bf9f3f
SAHIL015 <singhsahil16438@gmail.com>
2023-05-01 02:29:58 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import openai
+import os
+
+# Set your OpenAI API key
+openai.api_key = "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"
+
+# Define the prompt for the chatbot
+prompt = "Hello, how can I assist you today?"
+
+# Set the chatbot model and parameters
+model_engine = "text-davinci-002"
+temperature = 0.5
+max_tokens = 50
+
+# Define a function to generate a response from the chatbot
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens
+    )
+    message = response.choices[0].text.strip()
+    return message
+
+# Start the chatbot loop
+while True:
+    # Get user input
+    user_input = input("User: ")
+
+    # Exit loop if user says "bye"
+    if user_input.lower() == "bye":
+        break
+
+    # Generate and print the chatbot's response
+    chatbot_response = generate_response(prompt + "\nUser: " + user_input + "\nBot:")
+    print("Bot: " + chatbot_response)
